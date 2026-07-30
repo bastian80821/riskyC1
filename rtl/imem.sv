1028ce911f385a9c826ca84c505cec5ee956659b
Your Name <you@example.com>
2026-07-30 14:54:47 +1000

    Load programs from hex files via readmemh; add test programs

--- rtl/imem.sv
+++ rtl/imem.sv
@@ -11,11 +11,9 @@ module imem (
 
     //currently used to load a program
     initial begin
-    imem[0] = 32'h02A00093;  // addi x1, x0, 42    x1 = 42
-    imem[1] = 32'h00800113;  // addi x2, x0, 8     x2 = 8
-    imem[2] = 32'h00112023;  // sw   x1, 0(x2)     mem[8] = 42
-    imem[3] = 32'h00012183;  // lw   x3, 0(x2)     x3 = mem[8]
-    imem[4] = 32'h00000013;  // nop
+    for (int i = 0; i < 256; i++) imem[i] = 32'd0;
+        $readmemh("C:/Users/Bmars/Desktop/riskyC1/riskyC1/riscyC1/programs/alu_test.hex", imem); //ABSOLUTE PATH
+
     end
 
 
